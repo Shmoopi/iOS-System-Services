61c6bd14a75a60f1a77c4735b9ebba418b7f1f17
Evgeniy <Ewg777@users.noreply.github.com>
2016-09-01 10:26:21 +0300

    Update SSAccelerometerInfo.m
    Extension support added

--- System Services/Utilities/SSAccelerometerInfo.m
+++ System Services/Utilities/SSAccelerometerInfo.m
@@ -45,16 +45,18 @@
 + (UIInterfaceOrientation)deviceOrientation {
     // Get the device's current orientation
     @try {
-        // Device orientation
-        UIInterfaceOrientation Orientation = [[UIApplication sharedApplication] statusBarOrientation];
+        #if !(defined(__has_feature) && __has_feature(attribute_availability_app_extension))
+            // Device orientation
+            UIInterfaceOrientation Orientation = [[UIApplication sharedApplication] statusBarOrientation];
         
-        // Successful
-        return Orientation;
+            // Successful
+            return Orientation;
+        #end
     }
     @catch (NSException *exception) {
-        // Error
-        return -1;
     }
+    // Error
+    return -1;
 }
 
 // Start logging motion data
