61efebc3a66bf81e4f710fe7f7e148eed797cb78
Pawel Kania <pwlkania@gmail.com>
2016-09-30 23:52:11 +0200

    Added missing imports

--- System Services/iOSSystemServices.h
+++ System Services/iOSSystemServices.h
@@ -16,4 +16,18 @@ FOUNDATION_EXPORT const unsigned char iOSSystemServicesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <iOSSystemServices/PublicHeader.h>
 
-
+#import <iOSSystemServices/SystemServices.h>
+#import <iOSSystemServices/SSAccelerometerInfo.h>
+#import <iOSSystemServices/SSAccessoryInfo.h>
+#import <iOSSystemServices/SSApplicationInfo.h>
+#import <iOSSystemServices/SSBatteryInfo.h>
+#import <iOSSystemServices/SSCarrierInfo.h>
+#import <iOSSystemServices/SSDiskInfo.h>
+#import <iOSSystemServices/SSHardwareInfo.h>
+#import <iOSSystemServices/SSJailbreakCheck.h>
+#import <iOSSystemServices/SSLocalizationInfo.h>
+#import <iOSSystemServices/SSMemoryInfo.h>
+#import <iOSSystemServices/SSNetworkInfo.h>
+#import <iOSSystemServices/SSProcessInfo.h>
+#import <iOSSystemServices/SSProcessorInfo.h>
+#import <iOSSystemServices/SSUUID.h>
