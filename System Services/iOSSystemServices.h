83dab0ee4e97b8fa069ea826423e313a83200f7c
Pawel Kania <pwlkania@gmail.com>
2016-09-30 23:14:41 +0200

    Added Carthage support

---
+++ System Services/iOSSystemServices.h
@@ -0,0 +1,19 @@
+//
+//  iOSSystemServices.h
+//  iOSSystemServices
+//
+//  Created by Pawel Kania on 30/09/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSSystemServices.
+FOUNDATION_EXPORT double iOSSystemServicesVersionNumber;
+
+//! Project version string for iOSSystemServices.
+FOUNDATION_EXPORT const unsigned char iOSSystemServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSSystemServices/PublicHeader.h>
+
+
